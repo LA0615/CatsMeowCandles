03719c12b8553f457c888a0ead232f4a14297a4f
unknown <live2run_1980@yahoo.com>
2024-08-14 23:01:36 -0400

    homepage and header links

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,19 @@
-import RouterProvider, { createBrowserRouter } from "react-router-dom";
+import {RouterProvider,createBrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {path: '/', 
-  element: <App />,  // App is a react component
+  element: <App />, 
   children: [
-
+{index: true, element: <Home />}, 
 
   ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} /> // router is a react-router instance
+  <RouterProvider router={router} /> 
 );
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+//import Footer from "../components/Footer";
+
+//TO DO:
+//STYLE THE HOME PAGE
+//ADD A CAROUSEL
+//ADD A FEATURED PRODUCT SECTION
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Cat&apos;s Meow Candles</h1>
+      <p>Home page content goes here!</p>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Home;
