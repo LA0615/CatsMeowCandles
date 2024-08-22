0d2dc01ce1bff906abd4c392c5efb628c33fe5bf
unknown <live2run_1980@yahoo.com>
2024-08-22 00:50:27 -0400

    privacy policy and terms

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,49 +1,14 @@
-// import  { useState, useEffect } from 'react';
-// //import './Shop.css'; // Import page-specific CSS
+//import  { useState, useEffect } from 'react';
+import './Shop.css'; // Import page-specific CSS
 
 
 
 
-// const Shop = () => {
+const Shop = () => {
 
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-//       // Fetch products from an API or a static file
-//       const fetchProducts = async () => {
-//         try {
-//           const response = await fetch('/path/to/your/products/api');
-//           const data = await response.json();
-//           setProducts(data);
-//         } catch (error) {
-//           console.error('Error fetching products:', error);
-//         }
-//       };
-  
-//       fetchProducts();
-//     }, []);
-//     return (
-//         <div className="shop-container">
-         
-//           <main className="shop-main">
-//             <h1>Our Products</h1>
-//             <div className="products-grid">
-//               {products.map((product) => (
-//                 <div key={product.id} className="product-card">
-//                   <img src={product.image} alt={product.name} className="product-image" />
-//                   <h2 className="product-name">{product.name}</h2>
-//                   <p className="product-description">{product.description}</p>
-//                   <p className="product-price">${product.price}</p>
-//                 </div>
-//               ))}
-//             </div>
-//           </main>
-        
-//         </div>
-//       );
-//     };
+}
     
-//     export default Shop;
+    export default Shop;
 
 
 
