042c8a717f784770d18e3cd697a7bcaaee9a2626
unknown <live2run_1980@yahoo.com>
2025-03-17 20:41:00 -0400

    shop page

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -5,12 +5,12 @@
 //FUNCTION TO ADD ITEMS TO CART WHEN SELECTED or a button to add to cart
 
 
-
-
-//import  { useState, useEffect } from 'react';
-//import './Shop.css'; // Import page-specific CSS
-
+//import  { useState} from 'react';
+//import './Shop.css'; 
+//import Collapse from "react-bootstrap/Collapse";
+import "bootstrap/dist/css/bootstrap.min.css";
 //import CandleCare from "./CandleCare";
+//import ReturnPolicy from "./ReturnPolicy";
 
 
 const Shop = () => {
